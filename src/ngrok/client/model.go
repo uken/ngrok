38f586e7392746026c52365e23e505bfe96f8daf
devlin zed <devlin@uken.com>
2015-04-29 15:57:44 -0400

    remove debug code

--- src/ngrok/client/model.go
+++ src/ngrok/client/model.go
@@ -113,7 +113,6 @@ func newClientModel(config *Configuration, ctl mvc.Controller) *ClientModel {
 
 	// configure TLS SNI
 	m.tlsConfig.ServerName = serverName(m.serverAddr)
-	m.tlsConfig.InsecureSkipVerify = true
 
 	return m
 }
